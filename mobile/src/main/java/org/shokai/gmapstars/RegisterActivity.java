fe9d0cec7c67f48baa386a261ef7f26454a5ee85
Sho Hashimoto <hashimoto@shokai.org>
2015-04-13 01:39:42 +0900

    refact RegisterActivity

--- mobile/src/main/java/org/shokai/gmapstars/RegisterActivity.java
+++ mobile/src/main/java/org/shokai/gmapstars/RegisterActivity.java
@@ -1,10 +1,7 @@
 package org.shokai.gmapstars;
 
 import android.app.Activity;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnCancelListener;
 import android.content.Intent;
-import android.content.IntentSender.SendIntentException;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,7 +10,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.Result;
 import com.google.android.gms.common.api.ResultCallback;
@@ -29,90 +25,37 @@ public class RegisterActivity extends Activity implements
 
     private static final String TAG = "MainActivity";
 
-    private static final String KEY_IN_RESOLUTION = "is_in_resolution";
-
-    /**
-     * Request code for auto Google Play Services error resolution.
-     */
-    protected static final int REQUEST_CODE_RESOLUTION = 1;
-
-    /**
-     * Google API client.
-     */
     private GoogleApiClient mGoogleApiClient;
 
-    /**
-     * Determines if the client is in a resolution state, and
-     * waiting for resolution intent to return.
-     */
-    private boolean mIsInResolution;
-
-    private TextView textView;
-    private Button buttonAddGeofence, buttonRemoveGeofence;
+    private TextView mTextView;
+    private Button mButtonAddGeofence, mButtonRemoveGeofence;
     private GMap.Location location;
     private GeofenceManager geofenceManager;
+    private Intent mIntent;
 
-    /**
-     * Called when the activity is starting. Restores the activity state.
-     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        if (savedInstanceState != null) {
-            mIsInResolution = savedInstanceState.getBoolean(KEY_IN_RESOLUTION, false);
+
+        mIntent = this.getIntent();
+        if(mIntent == null || !mIntent.getAction().equals(Intent.ACTION_SEND)){
+            Log.e(TAG, "invalid Intent");
+            Toast.makeText(this, "invalid Intent", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
         }
 
-        textView = (TextView) findViewById(R.id.textView);
-        buttonAddGeofence = (Button) findViewById(R.id.buttonAddGefence);
-        buttonRemoveGeofence = (Button) findViewById(R.id.buttonRemoveGefence);
-
-
-        Intent intent = this.getIntent();
-        if(intent == null || !intent.getAction().equals(Intent.ACTION_SEND)) return;
-        GMap.getLocation(intent, new GMap.LocationCallback() {
-            @Override
-            public void onSuccess(GMap.Location location) {
-                Log.i("location", location.toString());
-                textView.setText(location.toString());
-                RegisterActivity.this.location = location;
-            }
-
-            @Override
-            public void onError(Exception ex) {
-                Log.e(TAG, ex.getMessage());
-            }
-        });
-
-        // Geofenceを登録する
-        buttonAddGeofence.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(location == null || geofenceManager == null) return;
-                geofenceManager.add(location, new ResultCallback() {
-                    @Override
-                    public void onResult(Result result) {
-                        Toast.makeText(RegisterActivity.this, result.getStatus().toString(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-            }
-        });
-
+        mTextView = (TextView) findViewById(R.id.textView);
+        mButtonAddGeofence = (Button) findViewById(R.id.buttonAddGefence);
+        mButtonRemoveGeofence = (Button) findViewById(R.id.buttonRemoveGefence);
     }
 
-    /**
-     * Called when the Activity is made visible.
-     * A connection to Play Services need to be initiated as
-     * soon as the activity is visible. Registers {@code ConnectionCallbacks}
-     * and {@code OnConnectionFailedListener} on the
-     * activities itself.
-     */
     @Override
     protected void onStart() {
         super.onStart();
         if (mGoogleApiClient == null) {
             mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    // Optionally, add additional APIs and scopes if required.
                     .addApi(LocationServices.API)
                     .addConnectionCallbacks(this)
                     .addOnConnectionFailedListener(this)
@@ -123,10 +66,6 @@ public class RegisterActivity extends Activity implements
         geofenceManager = new GeofenceManager(mGoogleApiClient, this);
     }
 
-    /**
-     * Called when activity gets invisible. Connection to Play Services needs to
-     * be disconnected as soon as an activity is invisible.
-     */
     @Override
     protected void onStop() {
         if (mGoogleApiClient != null) {
@@ -135,84 +74,46 @@ public class RegisterActivity extends Activity implements
         super.onStop();
     }
 
-    /**
-     * Saves the resolution state.
-     */
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        outState.putBoolean(KEY_IN_RESOLUTION, mIsInResolution);
-    }
-
-    /**
-     * Handles Google Play Services resolution callbacks.
-     */
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode) {
-            case REQUEST_CODE_RESOLUTION:
-                retryConnecting();
-                break;
-        }
-    }
-
-    private void retryConnecting() {
-        mIsInResolution = false;
-        if (!mGoogleApiClient.isConnecting()) {
-            mGoogleApiClient.connect();
-        }
-    }
-
-    /**
-     * Called when {@code mGoogleApiClient} is connected.
-     */
     @Override
     public void onConnected(Bundle connectionHint) {
         Log.i(TAG, "GoogleApiClient connected");
-        // TODO: Start making API requests.
+
+        GMap.getLocation(mIntent, new GMap.LocationCallback() {
+            @Override
+            public void onSuccess(GMap.Location location) {
+                Log.i("location", location.toString());
+                mTextView.setText(location.toString());
+                RegisterActivity.this.location = location;
+            }
+
+            @Override
+            public void onError(Exception ex) {
+                Log.e(TAG, ex.getMessage());
+            }
+        });
+
+        // Geofenceを登録する
+        mButtonAddGeofence.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (location == null || geofenceManager == null) return;
+                geofenceManager.add(location, new ResultCallback() {
+                    @Override
+                    public void onResult(Result result) {
+                        Toast.makeText(RegisterActivity.this, result.getStatus().toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
     }
 
-    /**
-     * Called when {@code mGoogleApiClient} connection is suspended.
-     */
     @Override
     public void onConnectionSuspended(int cause) {
         Log.i(TAG, "GoogleApiClient connection suspended");
-        retryConnecting();
     }
 
-    /**
-     * Called when {@code mGoogleApiClient} is trying to connect but failed.
-     * Handle {@code result.getResolution()} if there is a resolution
-     * available.
-     */
     @Override
     public void onConnectionFailed(ConnectionResult result) {
         Log.i(TAG, "GoogleApiClient connection failed: " + result.toString());
-        if (!result.hasResolution()) {
-            // Show a localized error dialog.
-            GooglePlayServicesUtil.getErrorDialog(
-                    result.getErrorCode(), this, 0, new OnCancelListener() {
-                        @Override
-                        public void onCancel(DialogInterface dialog) {
-                            retryConnecting();
-                        }
-                    }).show();
-            return;
-        }
-        // If there is an existing resolution error being displayed or a resolution
-        // activity has started before, do nothing and wait for resolution
-        // progress to be completed.
-        if (mIsInResolution) {
-            return;
-        }
-        mIsInResolution = true;
-        try {
-            result.startResolutionForResult(this, REQUEST_CODE_RESOLUTION);
-        } catch (SendIntentException e) {
-            Log.e(TAG, "Exception while starting resolution activity", e);
-            retryConnecting();
-        }
     }
 }
